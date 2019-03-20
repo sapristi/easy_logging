2b28232bd5e8564d28fee72c0e24ac4f4be449aa
sapristi <megabigfoutoir@gmail.com>
2019-03-20 23:38:12 +0100

    test

--- test/logging_test.ml
+++ test/logging_test.ml
@@ -1,19 +1,36 @@
 open Easy_logging
-       
+
+(* ************* *)
+(* Basic example *)
 let logger = Logging.make_logger
-               "test" (Some Info)
+               "test" (Some Debug)
                [Cli Debug];;
                
+logger#debug "This is a debug message";
+logger#info "This is an info message";
+logger#warning "This is a warning message";
+logger#error "This is an error message";
+logger#flash "This is a FLASH message";
 
-logger#info "ok";
-logger#warning "warn";
-logger#flash "FLASH";
-logger#debug "not displayed"
+(* another logger *)
+let sublogger = Logging.make_logger
+                  "test.sub" (Some Info)
+                  [Cli Debug]
+in
+
+(* ***************** *)
+(* log lazy messages *)
+let heavy_calculation () = "heavy result" in
+sublogger#ldebug (lazy (heavy_calculation ()));
 
 
-let logger2 = new Logging.logger "test2" (Some Debug)
-                [Cli Info];;
 
-logger2#warning "WARNI" ;
+(* ********************************** *)
+(* Globally modifying logger level :
+ * sets the level of all loggers whose
+ * name begins with "test"            *)
+Logging.set_level "test" (Some Warning);
 
-  
+
+(* ***************************** *)
+(* Custom handlers *)
