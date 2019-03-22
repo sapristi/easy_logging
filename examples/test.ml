ce9f5d1f811842d4606f21f99b1ea54628ca09c7
sapristi <megabigfoutoir@gmail.com>
2019-03-22 01:22:30 +0100

    examples and test

--- examples/test.ml
+++ examples/test.ml
@@ -1,3 +1,13 @@
 open Logging_examples
-
+open Easy_logging
        
+
+let logger = Logging.make_logger
+               "test" (Some Debug)
+               [Cli Debug];;
+               
+logger#debug "This is a debug message";
+logger#info "This is an info message";
+logger#warning "This is a warning message";
+logger#error "This is an error message";
+logger#flash "This is a FLASH message";
