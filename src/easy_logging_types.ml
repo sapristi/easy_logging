cb7feeeab5b7b4dfc0825824a39506397ec5918f
sapristi <megabigfoutoir@gmail.com>
2019-03-31 23:15:09 +0200

    load_config

--- src/easy_logging_types.ml
+++ src/easy_logging_types.ml
@@ -10,8 +10,24 @@ type level =
   | NoLevel
 [@@deriving show { with_path = false }]
 
- 
+let level_to_yojson lvl : Yojson.Safe.json =
+  `String (show_level lvl)
+let level_of_yojson lvl_json =
+  match lvl_json with
+  | `String lvl_str ->
+     (
+       match String.lowercase_ascii lvl_str with
+       | "debug" -> Ok Debug
+       | "info" -> Ok Info
+       | "warning" -> Ok Warning
+       | "error" -> Ok Error
+       | "flash" -> Ok Flash
+       | "nolevel" -> Ok NoLevel
+       | _ -> Error (lvl_str ^ " does not represent a valid log level")
+     )
+  | _ -> Error ("Cannot decode "^ (Yojson.Safe.to_string lvl_json) ^" to log level")
 
+       
 module type HandlersT =
   sig
     
@@ -33,7 +49,7 @@ module type HandlersT =
 
     (** Type used to instantiate a handler*)
     type desc
-
+       [@@deriving yojson]
     (** Instantiates a handler *)
     val make : desc -> t
 
