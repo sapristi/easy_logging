f283448e2b821409e4bd7457d6b6eb579fb55508
sapristi <megabigfoutoir@gmail.com>
2019-04-04 22:35:32 +0200

    fix

--- conf_loader/yojson/easy_logging_yojson.ml
+++ conf_loader/yojson/easy_logging_yojson.ml
@@ -4,18 +4,7 @@ module E = Easy_logging
    
 type log_level = E.log_level
                    [@@deriving show]
-
-let log_level_of_string lvl_string = 
-  match String.lowercase_ascii lvl_string with
-  | "debug" -> Ok (Debug :  log_level)
-  | "trace" -> Ok Trace
-  | "info" -> Ok Info
-  | "warning" -> Ok Warning
-  | "error" -> Ok Error
-  | "flash" -> Ok Flash
-  | "nolevel" -> Ok NoLevel
-  | _ -> Error (lvl_string ^ " does not represent a valid log level")
-       
+let log_level_of_string = E.log_level_of_string
 let log_level_to_yojson lvl : Yojson.Safe.json =
   `String (E.show_log_level lvl)
 let log_level_of_yojson lvl_json =
